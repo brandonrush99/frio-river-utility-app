d7dff09ecabc94fc4c75703b4c81d9020f1c4ae5
Brandon Rush <brush@webcreek.com>
2021-05-23 19:08:21 -0500

    Change weather api to allow https

--- components/weather.js
+++ components/weather.js
@@ -6,7 +6,6 @@ import { Dimensions } from 'react-native';
 
 export class Weather extends Component {
     _isMounted = false;
-    currentTime = new Date().getHours();
 
     constructor(props){
         super(props)
@@ -17,19 +16,24 @@ export class Weather extends Component {
     }
 
     componentDidMount() {
-        this._isMounted = true;
-        fetch('http://api.weatherstack.com/current?access_key=9e50a663b9fe95d5667241b215dc1c3c&query=78838&units=f')
-          .then((response) => response.json())
-          .then((json) => {
-            if (this._isMounted){
-              //console.log(json);
-              this.setState({ data: json });
-            }
-          })
-          .catch((error) => console.error(error))
-          .finally(() => {
-            this.setState({ isLoading: false });
+        let url = 'https://api.weather.gov/gridpoints/EWX/87,62/forecast';
+        fetch(url, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/geo+json',
+            'User-Agent': 'FrioWatch'
+          }
+        })
+        .then((response) => response.json())
+        .then((json) => {
+          this.setState({data: json}, () => {
+            this._isMounted = true;
           });
+        })
+        .catch((error) => console.error(error))
+        .finally(() => {
+          this.setState({ isLoading: false });
+        });
       }
     
       componentWillUnmount(){
@@ -38,49 +42,68 @@ export class Weather extends Component {
    
 
     render(){
-        const data = this.state.data;
         const isLoading = this.state.isLoading;
+        const data = this.state.data;
         let connectionIssue = false;
-        let temperature = -1;
-        try {
-          temperature = data.current.temperature;
-        } catch (error) {
-          connectionIssue = true;
+        let temperatureNow = -1;
+        let temperatureLater = -1;
+        let precipitationNow = '';
+        let precipitationLater = '';
+        let forecastNow = 'None';
+        //let forecastLater = 'None';
+        let windspeed = 'None';
+        let iconUrl = '';
+        let later = '';
+        let isDayTime = true;
+        if (this._isMounted === true){
+          try {
+            temperatureNow = data.properties.periods[0].temperature;
+            temperatureLater = data.properties.periods[1].temperature;
+            windspeed = data.properties.periods[0].windSpeed;
+            forecastNow = data.properties.periods[0].shortForecast;
+            //forecastLater = data.properties.periods[1].shortForecast;
+            iconUrl = data.properties.periods[0].icon;
+            let posNow = data.properties.periods[0].detailedForecast.indexOf('%');
+            let posLater = data.properties.periods[1].detailedForecast.indexOf('%');
+            precipitationNow = data.properties.periods[0].detailedForecast.slice(posNow-2,posNow+1);
+            precipitationLater = data.properties.periods[1].detailedForecast.slice(posLater-2,posLater+1);
+            later = data.properties.periods[1].name;
+            isDayTime = data.properties.periods[0].isDaytime;
+          } catch (error) {
+              console.log(error);
+              connectionIssue = true;
+          }
+          
         }
         return(
             <View style={styles.content}>
                 {isLoading ? <ActivityIndicator/> : (
                 <View>
                   {connectionIssue === false ? 
-                    <Card containerStyle={[styles.card, this.currentTime > 19 || this.currentTime < 7 ? styles.cardNight : styles.cardDay]}>
+                    <Card containerStyle={[styles.card, isDayTime === false ? styles.cardNight : styles.cardDay]}>
                       <View style={styles.container}>
-                        <Text h1 style={{color:'white'}}>{data.current.temperature}&#176;F</Text>
-                        <Text h4 style={{color:'white'}}>{data.current.weather_descriptions[0]}
-                          <Image source={{uri:data.current.weather_icons[0]}} style={styles.image}/>
-                        </Text>
+                        <Text h1 style={{color:'white'}}>{temperatureNow}&#176;F</Text>
+                        <Image source={{uri: iconUrl}} style={styles.image}/>  
                       </View>
-                      <Text style={styles.text}>Feels like {data.current.feelslike}&#176;F</Text>
-                      <Text style={styles.text}>{data.current.precip}% chance of rain</Text>
                       <Card.Divider style={{backgroundColor: 'white'}}/>
-                      <Text style={styles.update}>Last updated: {format(parseISO(data.location.localtime), 'MM/dd/yyyy hh:mm a')}</Text>
+                      <Text style={styles.textNow}>{forecastNow}</Text>
+                      <Text style={styles.textNow}>{windspeed} wind</Text>
+                      <Text style={styles.textNow}>{precipitationNow} chance of rain</Text>
+                      <Card.Divider style={{backgroundColor: 'white'}}/>
+                      <Text style={styles.textLater}>{later}: {temperatureLater}&#176;F and {precipitationLater} chance of rain</Text>
                     </Card> :
                     <Card containerStyle={[styles.card, this.currentTime > 19 || this.currentTime < 7 ? styles.cardNight : styles.cardDay]}>
                       <View style={styles.container}>
                         <Text h1 style={{color:'white'}}>{temperature}&#176;F</Text>
                         <Text h4 style={{color:'white'}}>None</Text>
                       </View>
-                      <Text style={styles.text}>Feels like -1&#176;F</Text>
-                      <Text style={styles.text}>-1% chance of rain</Text>
+                      <Text style={styles.textNow}>Feels like -1&#176;F</Text>
+                      <Text style={styles.textNow}>-1% chance of rain</Text>
                       <Card.Divider style={{backgroundColor: 'white'}}/>
                     </Card>
                   }
-                  
-
-          
                 </View>
-                )}
-               
-                
+                )} 
             </View>
         )
     }
@@ -114,8 +137,13 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
   },
-  text: {
+  textNow: {
     fontSize: 17,
-    color: 'white'
+    color: 'white',
+  },
+  textLater: {
+    fontSize: 15,
+    color: 'white',
+    alignSelf: 'center'
   }
 })
